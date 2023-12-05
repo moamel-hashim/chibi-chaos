abf615c1da3e9a6def75462e1e219d908587827d
moamel-hashim <muamal1997@gmail.com>
2023-12-04 22:54:51 -0800

    completed creating the main-page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { trigger, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  animations: [
+    trigger('fadeInLetters', [
+      transition(':enter', [
+        style({opacity: 0 }),
+        animate('1000ms', style({opacity: 100})),
+      ]),
+    ]),
+  ],
 })
+
 export class AppComponent {
-  title = 'chibi-chaos';
+  title = 'Welcome to Chibi Chaos';
+
+  display = false;
+  onPress() {
+    this.display = true
+  }
+  splitTitle(): string[] {
+    return this.title.split('');
+  }
 }
