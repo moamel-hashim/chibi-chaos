ba0be64b9eec30e32d809717eb7e841aa809bfdd
moamel-hashim <muamal1997@gmail.com>
2023-12-06 01:02:25 -0800

    completed adding side-module

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,13 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { faArrowLeft, faArrowRight, faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { trigger, transition, style, animate } from '@angular/animations';
 @Component({
   selector: 'app-main-page',
   standalone: true,
   imports: [CommonModule, FontAwesomeModule],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss',
+  animations: [
+    trigger('slideInOut',[
+      transition(':enter', [
+        style({transform: 'translateX(100%)'}),
+        animate('300ms ease-in-out', style({transform: 'translateX(0%)'})),
+      ]),
+      transition(':leave', [
+        animate('300ms ease-in-out', style({transform: 'translateX(100%)'})),
+      ]),
+    ]),
+  ],
 })
 export class MainPageComponent implements OnInit {
   title = 'Main Page';
@@ -15,6 +27,9 @@ export class MainPageComponent implements OnInit {
   data: any;
   faArrowLeft = faArrowLeft;
   faArrowRight = faArrowRight;
+  faChevronRight = faChevronRight;
+  faChevronLeft = faChevronLeft;
+  isModuleOpen = true;
 
   ngOnInit(): void {
     fetch(this.url)
@@ -31,5 +46,9 @@ export class MainPageComponent implements OnInit {
   generateStars(score:number): number[] {
     const roundedScore = Math.round(score);
     return Array.from({length: 10}, (_, index) => index < roundedScore ? 1 : 0);
+  }
+  toggleModule() {
+    this.isModuleOpen = !this.isModuleOpen;
+    console.log(this.isModuleOpen)
   }
 }
